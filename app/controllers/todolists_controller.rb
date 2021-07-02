60abd85d3766036cf3463127254cbb77c52d6d92
EC2 Default User <ec2-user@ip-172-31-45-222.ec2.internal>
2021-07-02 10:02:52 +0000

    [Finish]3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,13 +9,38 @@ class TodolistsController < ApplicationController
     list = List.new(list_params)
     # 2. データをデータベースに保存するためのsaveメゾッド実行
     list.save
-    # 3. トップ画面へのリダイレクト
-    redirect_to '/top'
+    # 3. トップ画面へのリダイレクトを変更
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を１件取得
+    list.destroy # データ（レコード）を消去
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0e31b091d692f637ae7e69f42dae49260c4c2156610afdc57c1bfb979525d5ba54c0dd78be86559abd1519da32e308d9d1282c5654dc3c20b63d9d004e369dc1'
